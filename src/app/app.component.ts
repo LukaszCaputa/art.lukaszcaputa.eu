d4ef12274d89890958c7646a60a6e5ec9526565d
LukaszCaputa <lukasz@lukaszcaputa.eu>
2024-01-05 18:18:33 +0100

    Added scully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, ScullyLibModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
