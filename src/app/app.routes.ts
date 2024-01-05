f9ef243b8511168f6cbec8b9f428c6212830c223
LukaszCaputa <lukasz@lukaszcaputa.eu>
2024-01-05 18:07:47 +0100

    Added components for content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ItemOverviewComponent } from './item-overview/item-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'items', component: ItemOverviewComponent },
+    { path: '', redirectTo: '/items', pathMatch: 'full' }
+
+];
---
+++ src/app/item-overview/item-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-overview.component.html',
+  styleUrl: './item-overview.component.css'
+})
+export class ItemOverviewComponent {
+
+}
